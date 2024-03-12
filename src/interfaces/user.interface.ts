3fdb7d333367cde6ac1e70c7227f6dd43d3bfaf7
moises-herrera <moisesherrera037@gmail.com>
2024-03-11 22:17:00 -0500

    chore: add user model

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,19 @@
+/**
+ * Represents the user information.
+ */
+export interface User {
+  /** The user's id. */
+  id: string;
+
+  /** The user's first name. */
+  firstName: string;
+
+  /** The user's last name. */
+  lastName: string;
+
+  /** The user's username. */
+  username: string;
+
+  /** The user's email. */
+  email: string;
+}
