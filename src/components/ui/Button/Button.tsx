4ebfc343ac94dc282174ac40e62bd87221f77bcb
moises-herrera <moisesherrera037@gmail.com>
2024-03-11 22:20:28 -0500

    feat: add loading spinner in button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import {
+  ActivityIndicator,
   GestureResponderEvent,
   Pressable,
   StyleProp,
   Text,
+  View,
   ViewStyle,
 } from 'react-native';
 import { styles } from './styles';
@@ -11,13 +13,34 @@ import { styles } from './styles';
 interface ButtonProps {
   label: string;
   onPress: (event: GestureResponderEvent) => void;
-  style?: { button: StyleProp<ViewStyle>; buttonText: StyleProp<ViewStyle> };
+  disabled?: boolean;
+  isLoading?: boolean;
+  style?: {
+    button: StyleProp<ViewStyle>;
+    buttonText: StyleProp<ViewStyle>;
+    disabled: StyleProp<ViewStyle>;
+  };
 }
 
-export const Button: FC<ButtonProps> = ({ label, onPress, style = styles }) => {
+export const Button: FC<ButtonProps> = ({
+  label,
+  onPress,
+  disabled = false,
+  isLoading = false,
+  style = styles,
+}) => {
   return (
-    <Pressable style={style.button} onPress={onPress}>
-      <Text style={style.buttonText}>{label}</Text>
+    <Pressable
+      style={[style.button, (disabled || isLoading) && style.disabled]}
+      onPress={onPress}
+      disabled={disabled || isLoading}
+    >
+      <View style={styles.content}>
+        {isLoading && (
+          <ActivityIndicator size="small" color={styles.buttonText.color} />
+        )}
+        <Text style={style.buttonText}>{label}</Text>
+      </View>
     </Pressable>
   );
 };
--- src/components/ui/Button/styles.ts
+++ src/components/ui/Button/styles.ts
@@ -1,13 +1,22 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../../theme';
 
 export const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'black',
+    backgroundColor: colors.primary,
     padding: 12,
     borderRadius: 6,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  content: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 6,
+  },
   buttonText: { color: 'white' },
+  disabled: {
+    opacity: 0.6,
+  },
 });
