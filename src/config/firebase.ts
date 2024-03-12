e9898c638185d9bb4b6dab962a2833472c6d19af
moises-herrera <moisesherrera037@gmail.com>
2024-03-12 17:33:02 -0500

    refactor: reuse constant in firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,14 +3,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+const expoExtraConfig = Constants.expoConfig?.extra;
+
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: Constants.expoConfig?.extra?.apiKey,
-  authDomain: Constants.expoConfig?.extra?.authDomain,
-  projectId: Constants.expoConfig?.extra?.projectId,
-  storageBucket: Constants.expoConfig?.extra?.storageBucket,
-  messagingSenderId: Constants.expoConfig?.extra?.messagingSenderId,
-  appId: Constants.expoConfig?.extra?.appId,
+  apiKey: expoExtraConfig?.apiKey,
+  authDomain: expoExtraConfig?.authDomain,
+  projectId: expoExtraConfig?.projectId,
+  storageBucket: expoExtraConfig?.storageBucket,
+  messagingSenderId: expoExtraConfig?.messagingSenderId,
+  appId: expoExtraConfig?.appId,
 };
 
 // Initialize Firebase
