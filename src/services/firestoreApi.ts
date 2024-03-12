e1d41f1a542153fa2540e08ab713358a7e3c4837
moises-herrera <moisesherrera037@gmail.com>
2024-03-12 17:34:06 -0500

    refactor: reuse user service in firestore api

--- src/services/firestoreApi.ts
+++ src/services/firestoreApi.ts
@@ -1,12 +1,7 @@
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from 'firebase/auth';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
-import { firebaseAuth, firebaseDatabase } from '../config/firebase';
 import { LoginSchemaType, SignUpSchemaType } from '../schemas/auth';
-import type { User as UserInfo } from '../interfaces';
+import { loginUser, registerUser } from './user.service';
+import type { AuthInfo } from '../interfaces';
 
 /**
  * Represents the firestore api.
@@ -16,74 +11,15 @@ export const firestoreApi = createApi({
   baseQuery: fakeBaseQuery(),
   tagTypes: ['User'],
   endpoints: (builder) => ({
-    loginUser: builder.mutation<
-      {
-        user: UserInfo;
-        token: string;
-      },
-      LoginSchemaType
-    >({
+    loginUser: builder.mutation<AuthInfo, LoginSchemaType>({
       async queryFn(data) {
-        try {
-          const credentials = await signInWithEmailAndPassword(
-            firebaseAuth,
-            data.email,
-            data.password
-          );
-          const userDocument = await getDoc(
-            doc(firebaseDatabase, 'users', credentials.user.uid)
-          );
-          const user = {
-            id: userDocument.id,
-            ...userDocument.data(),
-          } as UserInfo;
-
-          const token = await credentials.user.getIdToken();
-          return {
-            data: {
-              user,
-              token,
-            },
-          };
-        } catch (error) {
-          console.error(error);
-          return { error: 'Error al iniciar sesión' };
-        }
+        return await loginUser(data);
       },
       invalidatesTags: ['User'],
     }),
-    registerUser: builder.mutation<
-      {
-        user: UserInfo;
-        token: string;
-      },
-      SignUpSchemaType
-    >({
+    registerUser: builder.mutation<AuthInfo, SignUpSchemaType>({
       async queryFn(data) {
-        try {
-          const credentials = await createUserWithEmailAndPassword(
-            firebaseAuth,
-            data.email,
-            data.password
-          );
-
-          const { confirmPassword, password, ...userInfo } = data;
-
-          await setDoc(
-            doc(firebaseDatabase, 'users', credentials.user.uid),
-            userInfo
-          );
-          const token = await credentials.user.getIdToken();
-          const user = {
-            id: credentials.user.uid,
-            ...userInfo,
-          } as UserInfo;
-
-          return { data: { user, token } };
-        } catch (error) {
-          console.error(error);
-          return { error: 'Error al registrar el usuario' };
-        }
+        return await registerUser(data);
       },
       invalidatesTags: ['User'],
     }),
