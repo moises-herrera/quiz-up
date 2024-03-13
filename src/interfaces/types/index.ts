293bebce79cbe8002752e9869a3fdea6e3378428
moises-herrera <moisesherrera037@gmail.com>
2024-03-12 22:02:52 -0500

    chore: add toast message types

--- src/interfaces/types/index.ts
+++ src/interfaces/types/index.ts
@@ -1,3 +1,5 @@
 export * from './form-submit-handler.type';
 export * from './fields-validation-options.type';
 export * from './api-standard-response.type';
+export * from './toast-message.type';
+export * from './toast-styles.type';
