8fa7dc3e9c9beed2ca69c467769bbf8c893ef764
moises-herrera <moisesherrera037@gmail.com>
2024-03-12 22:03:30 -0500

    refactor: reuse toast message type

--- src/components/ui/Toast/Toast.tsx
+++ src/components/ui/Toast/Toast.tsx
@@ -1,17 +1,20 @@
 import { FC } from 'react';
 import { Text, View } from 'react-native';
 import { toastStyles } from './styles';
+import { ToastMessage } from '../../../interfaces';
 
 interface ToastProps {
   message: string;
-  type: 'error' | 'success';
+  type: ToastMessage;
   onClose?: () => void;
 }
 
 export const Toast: FC<ToastProps> = ({ message, type, onClose }) => {
   return (
     <View style={toastStyles[type].container}>
-      <Text style={toastStyles[type].text}>{message}</Text>
+      <View style={toastStyles[type].messageContainer}>
+        <Text style={toastStyles[type].text}>{message}</Text>
+      </View>
     </View>
   );
 };
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,4 @@ export * from './Input';
 export * from './Button';
 export * from './FormControl';
 export * from './WrapperBase';
+export * from './Toast';
