9ec82aa5ae29598eb6558b07dd2825c3cf431a20
moises-herrera <moisesherrera037@gmail.com>
2024-03-22 18:08:34 -0500

    chore: add ui slice

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { firestoreApi } from '../../services';
 import { authSlice } from '../auth';
+import { uiSlice } from '../ui';
 
 /** Redux store. */
 export const store = configureStore({
   reducer: {
     [firestoreApi.reducerPath]: firestoreApi.reducer,
     auth: authSlice.reducer,
+    ui: uiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(firestoreApi.middleware),
