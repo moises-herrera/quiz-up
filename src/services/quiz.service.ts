d2f5d13287fdd3795ed679a0bc7078fdde4d2cc8
moises-herrera <moisesherrera037@gmail.com>
2024-03-23 17:12:58 -0500

    chore: add quiz service

---
+++ src/services/quiz.service.ts
@@ -0,0 +1,40 @@
+import { doc, setDoc } from 'firebase/firestore';
+import { APIStandardResponse, Quiz } from '../interfaces';
+import { firebaseDatabase } from '../config/firebase';
+import { handleError } from '../helpers';
+import { uploadFile } from './file.service';
+
+/**
+ * Creates a new quiz.
+ *
+ * @param quiz The quiz data.
+ * @returns A promise with the quiz data.
+ */
+export const createQuiz = async (quiz: Quiz) => {
+  try {
+    const { id, questions, ...data } = quiz;
+
+    const imageUrl = await uploadFile('quizzes', quiz.image as Blob);
+
+    await setDoc(doc(firebaseDatabase, 'quizzes', id), {
+      ...data,
+      image: imageUrl,
+    });
+    const questionPromises = questions.map((question) => {
+      return setDoc(
+        doc(firebaseDatabase, 'quizzes', id, 'questions', question.id),
+        question
+      );
+    });
+    await Promise.all(questionPromises);
+
+    const response: APIStandardResponse<Quiz> = {
+      data: quiz,
+    };
+
+    return response;
+  } catch (error) {
+    const message = handleError(error, 'Error al crear el quiz');
+    return { error: message };
+  }
+};
