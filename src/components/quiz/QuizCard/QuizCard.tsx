e049ab4e6417e644e39c2c3ef5fe0021075c7416
moises-herrera <moisesherrera037@gmail.com>
2024-03-24 22:15:09 -0500

    chore: add quiz card actions

--- src/components/quiz/QuizCard/QuizCard.tsx
+++ src/components/quiz/QuizCard/QuizCard.tsx
@@ -1,24 +1,80 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Image, Text, View } from 'react-native';
-import { Quiz } from '../../../interfaces';
+import { NavigationProps, Quiz } from '../../../interfaces';
 import { styles } from './styles';
 import { Button } from '../../ui';
 import { Entypo } from '@expo/vector-icons';
-import { useAppSelector } from '../../../hooks';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { useNavigation } from '@react-navigation/native';
+import { removeQuiz, setActiveQuiz } from '../../../redux/quiz';
+import { Menu } from 'react-native-paper';
+import { useDeleteQuizMutation } from '../../../services';
+import { displayToast } from '../../../redux/ui';
 
-export const QuizCard: FC<Quiz> = ({ title, image, category, user }) => {
+export const QuizCard: FC<Quiz> = (quiz) => {
+  const { title, image, categoryLabel, user } = quiz;
+  const dispatch = useAppDispatch();
   const currentUser = useAppSelector(({ auth: { user } }) => user);
+  const navigation = useNavigation<NavigationProps>();
+  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+  const [deleteQuiz, { isSuccess, isError }] = useDeleteQuizMutation();
+
+  const handleEdit = () => {
+    dispatch(setActiveQuiz({ quiz }));
+    navigation.navigate('QuizSettings');
+  };
+
+  const handleDelete = async () => {
+    await deleteQuiz(quiz.id);
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(removeQuiz(quiz.id));
+      dispatch(
+        displayToast({
+          type: 'success',
+          message: 'Quiz eliminado correctamente',
+        })
+      );
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(
+        displayToast({
+          type: 'error',
+          message: 'Error al eliminar el quiz',
+        })
+      );
+    }
+  }, [isError]);
 
   return (
     <View style={styles.card}>
-      {currentUser?.username === user && (
-        <Entypo
-          style={{ alignSelf: 'flex-end', marginBottom: 8 }}
-          name="dots-three-vertical"
-          size={20}
-          color="black"
-        />
-      )}
+      {
+        <View style={{ alignSelf: 'flex-end' }}>
+          <Menu
+            style={{ marginTop: 30 }}
+            contentStyle={{ backgroundColor: 'white' }}
+            visible={isMenuVisible}
+            onDismiss={() => setIsMenuVisible(false)}
+            anchor={
+              <Entypo
+                style={{ alignSelf: 'flex-end', marginBottom: 8 }}
+                name="dots-three-vertical"
+                size={20}
+                color="black"
+                onPress={() => setIsMenuVisible(true)}
+              />
+            }
+          >
+            <Menu.Item title="Editar" onPress={handleEdit} />
+            <Menu.Item title="Eliminar" onPress={handleDelete} />
+          </Menu>
+        </View>
+      }
       <View style={{ gap: 6 }}>
         {
           <Image
@@ -30,7 +86,7 @@ export const QuizCard: FC<Quiz> = ({ title, image, category, user }) => {
             }
           />
         }
-        <Text style={styles.categoryLabel}>{category}</Text>
+        <Text style={styles.categoryLabel}>{categoryLabel}</Text>
         <Text style={styles.title}>{title}</Text>
       </View>
       <Text style={styles.username}>@{user}</Text>
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,17 +4,27 @@ import { useLazyGetQuizzesQuery } from '../../services';
 import { useEffect } from 'react';
 import { QuizList } from '../../components/quiz';
 import { LoadingScreen } from '../../components/ui';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { setQuizzes } from '../../redux/quiz';
 
 export const Home = () => {
+  const dispatch = useAppDispatch();
   const [getQuizzes, { data }] = useLazyGetQuizzesQuery();
+  const quizzes = useAppSelector(({ quiz: { quizzes } }) => quizzes);
 
   useEffect(() => {
     getQuizzes();
   }, []);
 
+  useEffect(() => {
+    if (data?.length) {
+      dispatch(setQuizzes(data));
+    }
+  }, [data]);
+
   return (
     <View style={styles.content}>
-      {data ? <QuizList quizList={data} /> : <LoadingScreen />}
+      {quizzes ? <QuizList quizList={quizzes} /> : <LoadingScreen />}
     </View>
   );
 };
